bf9004d980cb79e7d7b1a4a248bbde604b76435e
mickymalmazora <120387528+mickymalmazora@users.noreply.github.com>
2023-02-06 18:01:47 +0800

    Add files via upload

---
+++ SIS Initial Code.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+int main() {
+	cout << "Enter login name:\n ";
+
+	cout << "Enter password :\n";
+
+	cout << "Hello MA " ", Welcome to the ... \n";
+
+	cout << "\n\t STUDENT INFORMATION SYSTEM \n";
+
+	cout << "\n   \t\t\t 1. Add Records";
+
+	cout << "\n   \t\t\t 2. List Records";
+
+	cout << "\n   \t\t\t 3. Modify Records";
+
+	cout << "\n   \t\t\t 4. Delete Records";
+
+	cout << "\n   \t\t\t 5. Exit Program \n ";
+
+	cout << "\n    \t\t\t Select Your Choice:\n ";
+}
+
